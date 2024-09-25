34b91ba52b28426cd0d2c8ecdf9c3e607ea93b5b
godosint <142482025+godosint@users.noreply.github.com>
2024-09-25 14:21:30 +0200

    Update popupAvatars.js

--- assets/js/popupAvatars.js
+++ assets/js/popupAvatars.js
@@ -1,32 +1,43 @@
 async function fetchAvatarsForAll() {
-    const liElements = document.querySelectorAll('#popup li');
+  const liElements = document.querySelectorAll("#popup li");
+  const corsProxy = "https://cors-anywhere.herokuapp.com/";
 
-    for (let li of liElements) {
-        const imgElement = li.querySelector('img');
+  for (let li of liElements) {
+    const imgElement = li.querySelector("img");
 
-        if (imgElement) {
-            const userId = imgElement.alt;
+    if (imgElement) {
+      const userId = imgElement.alt;
 
-            imgElement.src = "assets/img/black.png";
+      imgElement.src = "assets/img/black.png";
 
-            if (userId) {
-                try {
-                    const response = await fetch(`https://185.228.81.59:3000/api/avatar/${userId}`);
-                    const data = await response.json();
+      if (userId) {
+        try {
+          let response = await fetch(
+            `https://api.wxrn.lol/api/avatar/${userId}`
+          );
 
-                    if (data.avatarUrl) {
-                        imgElement.src = data.avatarUrl;
-                    } else if (data.error) {
-                        console.error(`Error for user ${userId}: ${data.error}`);
-                    }
-                } catch (error) {
-                    console.error(`Error fetching avatar for user ${userId}:`, error);
-                }
-            } else {
-                console.error('No Discord User ID found in the alt attribute.');
-            }
+          if (!response.ok) {
+            response = await fetch(
+              `${corsProxy}https://api.wxrn.lol/api/avatar/${userId}`
+            );
+          }
+
+          const data = await response.json();
+
+          if (data.avatarUrl) {
+            imgElement.src = data.avatarUrl;
+          } else if (data.error) {
+            console.error(`Error for user ${userId}: ${data.error}`);
+          }
+        } catch (error) {
+          console.error(`Failed to fetch avatar for user ${userId}:`, error);
+          continue;
         }
+      } else {
+        console.error("No Discord User ID found in the alt attribute.");
+      }
     }
+  }
 }
 
 fetchAvatarsForAll();
