5f857ef7707d8665be6afe966d0572ff896da2fb
wxrned <wxrn.lol@gmail.com>
2025-03-02 15:28:40 +0100

    changes

--- assets/js/player.js
+++ assets/js/player.js
@@ -16,7 +16,7 @@ const lyricsCloseBtn = document.getElementById("lyrics-close");
 const lyricsButton = document.getElementById("lyrics-button");
 
 const API_URL = "https://api.wxrn.lol/api/lyrics";
-const defaultFooterText = "〤 @4kpx 〤";
+const defaultFooterText = "〤 praising shiku 〤";
 
 const tracks = [
   {
