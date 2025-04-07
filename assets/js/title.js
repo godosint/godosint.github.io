5fc39a52a32299710e361002480f4347df77130a
warn <wxrn609@gmail.com>
2025-04-07 14:14:37 +0200

    Update title.js

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,7 +1,7 @@
 async function fetchUsername() {
   try {
     let userId = "1138972279506747462";
-    const response = await fetch(`https://api.wxrn.lol/api/discord/${userId}`);
+    const response = await fetch(`https://api.wxrn.lol/discord/${userId}`);
     const data = await response.json();
 
     const username = `@${data.username}` || "@user";
